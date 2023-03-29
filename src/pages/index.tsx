facef3a1713985e0732316c622d1f89aa89bf332
Navin Moorthy <navin007.a@gmail.com>
2023-03-29 11:14:09 +0530

    chore(package.json): ⬆️ upgrade  dev dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => (
 					pages/index.tsx
 				</code>
 			</p>
-			<Button className="mt-3 mr-2 mb-2 rounded-lg border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+			<Button className="mb-2 mr-2 mt-3 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
 				Button Component
 			</Button>
 			<div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
