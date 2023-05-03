770027981d03bc24dc4dd609ed45d15b6d307fa8
Navin Moorthy <navin007.a@gmail.com>
2023-05-03 10:29:15 +0530

    chore(package): 📦 Upgrade next, @commitlint, eslint, stylelint, and other dependencies
    - Upgrade next from 13.3.0 to 13.3.4
    - Upgrade @commitlint/cli from 17.5.1 to 17.6.1
    - Upgrade @commitlint/config-conventional from 17.4.4 to 17.6.1
    - Upgrade @next/* from 13.3.0 to 13.3.4
    - Upgrade @types/node from 18.15.11 to 18.16.3
    - Upgrade @types/react from 18.0.34 to 18.2.0
    - Upgrade @types/react-dom from 18.0.11 to 18.2.1
    - Upgrade @typescript-eslint/experimental-utils from 5.58.0 to 5.59.2
    - Upgrade all-contributors-cli from 6.24.0 to 6.25.0
    - Upgrade autoprefixer from 10.4.14 to 10.4.15
    - Upgrade cspell from 6.31.1 to 6.32.0
    - Upgrade del-cli from 5.0.0 to 5.1.0
    - Upgrade eslint from 8.38.0 to 8.39.0
    - Upgrade eslint-config-canonical from 41.0.1 to 41.0.3
    - Upgrade markdownlint from 0.28.1 to 0.28.2
    - Upgrade markdownlint-cli from 0.33.0 to 0.34.0
    - Upgrade next-sitemap from 4.0.7 to 4.0.9
    - Upgrade postcss from 8.4.21 to 8.4.23
    - Upgrade prettier from 2.8.7 to 2.8.8
    - Upgrade prettier-plugin-tailwindcss from 0.2.7 to 0.2.8
    - Upgrade release-it from 15.10.1 to 15.10.3
    - Upgrade sharp from 0.32.0 to 0.32.1
    - Upgrade stylelint from 15.4.0 to 15.6.1
    - Upgrade stylelint-config-standard from 32.0.0 to 33.0.0
    - Upgrade tailwindcss from 3.3.1 to 3.3.2
    - Upgrade turbo from 1.8.8 to 1.9.3

--- env/schema.js
+++ env/schema.js
@@ -13,7 +13,6 @@ export const serverSchema = z.object({
 /**
  * You can't destruct `process.env` as a regular object in the Next.js
  * middleware, so you have to do it manually here.
- *
  * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
  */
 export const serverEnvironment = {
@@ -35,7 +34,6 @@ export const clientSchema = z.object({
  * You can't destruct `process.env` as a regular object, so you have to do
  * it manually here. This is because Next.js evaluates this at build time,
  * and only used environment variables are included in the build.
- *
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnvironment = {
